7ad7b0a5af514dd8dd88bd13888de97cc05564cd
sarapatiernoalten <sara.patierno@alten.it>
2023-05-12 10:53:55 +0200

    Some tests

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
+import animals.*;
+
+import java.util.Date;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    static Logger log = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
-        // Press Alt+Invio with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        log.info("Loading lions...");
 
-        // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        TailedAnimal l1 = new Lion("Gennaro", "carne di Zebra", 5, new Date(), 180, 120, 80);
+        TailedAnimal l2 = new Lion("Ciro", "carne di Gnu", 8, new Date(), 185, 100, 75);
+        TailedAnimal l3 = new Lion("Chanel", "carne di Gazzella", 8, new Date(), 110, 100, 77);
 
-            // Press Maiusc+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+        log.info("Loading tigers...");
+
+        TailedAnimal t1 = new Tiger("Rihanna", "carne di Cinghiale", 10, new Date(), 150, 200, 95);
+        TailedAnimal t2 = new Tiger("Jessica", "carne di Cervo", 13, new Date(), 175, 225, 85);
+        TailedAnimal t3 = new Tiger("Ludovico", "carne di Cinghiale", 16, new Date(), 200, 295, 90);
+
+
+        log.info("Loading eagles...");
+
+        WingedAnimal e1 = new Eagle("Jayson", "carne di Volpe", 34, new Date(), 4, 90, 200);
+        WingedAnimal e2 = new Eagle("Letizia", "carne di Coniglio", 10, new Date(), 3, 70, 190);
+        WingedAnimal e3 = new Eagle("Tommaso", "carne di Lepre", 18, new Date(), 3, 85, 210);
+
+
+        log.info("Loading done");
+
+        System.out.println("Lion name: "+l1.getName()+" - Tail length: "+l1.getTailLength()+" (expected: 80)");
+
     }
 }
